402381e22f400c1ecd9dc3ad99fc57f195246192
Wen-Ting Wang <egpivo@gmail.com>
2021-01-31 14:30:13 +0000

    version 1.3.3.0

---
+++ README.md
@@ -0,0 +1,59 @@
+## SpatPCA Package
+
+[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/SpatPCA)](https://CRAN.R-project.org/package=SpatPCA)
+[![Downloads](http://cranlogs.r-pkg.org/badges/grand-total/SpatPCA)](https://CRAN.R-project.org/package=SpatPCA)
+[![R build status](https://github.com/egpivo/SpatPCA/workflows/R-CMD-check/badge.svg)](https://github.com/egpivo/SpatPCA/actions)
+[![Coverage Status](https://img.shields.io/codecov/c/github/egpivo/SpatPCA/master.svg)](https://codecov.io/github/egpivo/SpatpCA?branch=master)
+
+### Description
+***SpatPCA*** is an R package that facilitates regularized principal component analysis, 
+
+* seeking the dominant patterns (eigenfunctions), which can be smooth and localized
+* computing spatial prediction (Kriging) at new locations
+* suitable for either regularly or irregularly spaced data, including 1D, 2D, and 3D
+* by the alternating direction method of multipliers (ADMM) algorithm
+
+
+### Installation
+To get the current released version from CRAN:
+
+```r
+install.packages("SpatPCA")
+```
+
+To get the current development version from GitHub:
+
+```r
+devtools::install_github("egpivo/SpatPCA")
+```
+To compile C++ code with the package [`RcppArmadillo`](https://CRAN.R-project.org/package=RcppArmadillo),
+
+ * Windows users require [Rtools](https://CRAN.R-project.org/bin/windows/Rtools/)
+ * Mac users require Xcode Command Line Tools, and install the library gfortran by typing the following lines into terminal
+
+```
+curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
+sudo tar fvxz gfortran-4.8.2-darwin13.tar.bz2 -C /
+```
+More details can be found [here](http://thecoatlessprofessor.com/programming/rcpp-rcpparmadillo-and-os-x-mavericks-lgfortran-and-lquadmath-error/).
+
+### Usage
+```r
+library(SpatPCA)
+spatpca(position, realizations)
+```
+- Input: realizations with the corresponding position
+- Output: return the most dominant eigenfunctions automatically.
+- More details can be referred to [Demo](https://egpivo.github.io/SpatPCA/articles/)
+
+### Author
+[Wen-Ting Wang](https://www.linkedin.com/in/wen-ting-wang-6083a17b "Wen-Ting Wang") and [Hsin-Cheng Huang](http://www.stat.sinica.edu.tw/hchuang/ "Hsin-Cheng Huang")
+ 
+### Maintainer
+[Wen-Ting Wang](https://www.linkedin.com/in/wen-ting-wang-6083a17b "Wen-Ting Wang")
+
+### Reference
+Wang, W.-T. and Huang, H.-C. (2017). [Regularized principal component analysis for spatial data](https://arxiv.org/pdf/1501.03221v3.pdf, "Regularized principal component analysis for spatial data"). *Journal of Computational and Graphical Statistics*, **26**, 14-25.
+ 
+### License
+  GPL-3
---
+++ tests/testthat/test-SpatPCA.R
@@ -0,0 +1,82 @@
+# generate 1-D data with a given seed
+set.seed(1234)
+tol <- 1e-6
+num_cores <- 2
+
+x_1D <- as.matrix(seq(-5, 5, length = 10))
+Phi_1D <- exp(-x_1D^2) / norm(exp(-x_1D^2), "F")
+Y_1D <- {
+  rnorm(n = 100, sd = 3) %*% t(Phi_1D) +
+    matrix(rnorm(n = 100 * 10), 100, 10)
+}
+
+cv_1D <- spatpca(x = x_1D, Y = Y_1D, num_cores = num_cores)
+cv_1D_fixed_K_multiple_tau2 <- spatpca(x = x_1D, Y = Y_1D, K = 1, tau2 = c(0, 1), num_cores = num_cores)
+
+used_number_cores <- as.integer(Sys.getenv("RCPP_PARALLEL_NUM_THREADS", ""))
+expected_selected_tau1_R_3.6_higher <- 0.00046416
+expected_selected_tau1_R_3.6_lower <- 0.01
+expected_selected_gamma_R_3.6_higher <- 0.44503397
+expected_selected_gamma_R_3.6_lower <- 0.4737518
+
+# Test
+test_that("Selected tuning parameters", {
+  expect_lte(
+    min(
+      abs(cv_1D$selected_tau1 - expected_selected_tau1_R_3.6_higher),
+      abs(cv_1D$selected_tau1 - expected_selected_tau1_R_3.6_lower)
+    ),
+    tol
+  )
+  expect_lte(abs(cv_1D$selected_tau2 - 0), tol)
+  expect_lte(
+    min(
+      abs(cv_1D$selected_gamma - expected_selected_gamma_R_3.6_higher),
+      abs(cv_1D$selected_gamma - expected_selected_gamma_R_3.6_lower)
+    ),
+    tol
+  )
+  expect_null(cv_1D$selected_K)
+  expect_equal(cv_1D_fixed_K_multiple_tau2$selected_K, 1)
+  expect_lte(abs(cv_1D_fixed_K_multiple_tau2$selected_tau1 - 0.002154435), tol)
+  expect_equal(cv_1D_fixed_K_multiple_tau2$selected_tau2, 1)
+})
+
+test_that("Number of threads", {
+  expect_equal(num_cores, used_number_cores)
+})
+
+test_that("cross-validation plot", {
+  expect_error(
+    plot.spatpca("test"),
+    cat("Invalid object! Please enter a `spatpca` object")
+  )
+  expect_true("ggplot" %in% class(plot.spatpca(cv_1D)))
+})
+
+# Test `predict`
+x_1Dnew <- as.matrix(seq(6, 7, length = 4))
+prediction <- predict(cv_1D, x_new = x_1Dnew)
+dominant_pattern_on_new_sites <- predictEigenfunction(cv_1D, x_new = x_1Dnew)
+
+test_that("prediction", {
+  expect_equal(ncol(prediction), 4)
+  expect_equal(nrow(dominant_pattern_on_new_sites), 4)
+})
+
+
+# Test auxiliary function - CV with selecting K
+M <- 3
+shuffle_split <- sample(rep(1:M, length.out = nrow(Y_1D)))
+tau1 <- setTau1(NULL, M)
+tau2 <- setTau2(NULL, M)
+l2 <- setL2(tau2)
+setCores(2)
+cv_with_k_seleted <- spatpcaCVWithSelectingK(x_1D, Y_1D, M, tau1, tau2, 1, shuffle_split, 10, 1e-04, l2)
+
+test_that("auxiliary function for selecting K", {
+  expect_equal(cv_with_k_seleted$selected_K, 1)
+  expect_equal(cv_with_k_seleted$cv_result$selected_gamma, 1)
+  expect_equal(cv_with_k_seleted$cv_result$selected_tau1, 0)
+  expect_equal(cv_with_k_seleted$cv_result$selected_tau2, 0)
+})
